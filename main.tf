f23f67fea1580acd7b6c40d42d6aa7050e1daa9c
Tamas Sallai <gsashee@gmail.com>
2020-05-30 09:28:02 +0200

    initial

---
+++ main.tf
@@ -0,0 +1,96 @@
+provider "aws" {
+}
+
+resource "random_id" "id" {
+  byte_length = 8
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  output_path = "/tmp/lambda-${random_id.id.hex}.zip"
+  source {
+    content  = <<EOF
+module.exports.handler = async (event, context) => {
+	return {
+		statusCode: 200,
+		headers: {
+			"Content-Type": "text/html",
+		},
+		body: "Hello world!",
+	}
+};
+EOF
+    filename = "main.js"
+  }
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name = "${random_id.id.hex}-function"
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  handler = "main.handler"
+  runtime = "nodejs12.x"
+  role    = aws_iam_role.lambda_exec.arn
+}
+
+data "aws_iam_policy_document" "lambda_exec_role_policy" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "arn:aws:logs:*:*:*"
+    ]
+  }
+}
+
+resource "aws_cloudwatch_log_group" "loggroup" {
+  name              = "/aws/lambda/${aws_lambda_function.lambda.function_name}"
+  retention_in_days = 14
+}
+
+resource "aws_iam_role_policy" "lambda_exec_role" {
+  role   = aws_iam_role.lambda_exec.id
+  policy = data.aws_iam_policy_document.lambda_exec_role_policy.json
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+# api gw
+
+resource "aws_apigatewayv2_api" "api" {
+  name          = "api-${random_id.id.hex}"
+  protocol_type = "HTTP"
+  target        = aws_lambda_function.lambda.arn
+}
+
+resource "aws_lambda_permission" "apigw" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.arn
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+}
+
+output "url" {
+	value = aws_apigatewayv2_api.api.api_endpoint
+}
+
